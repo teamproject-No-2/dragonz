9e0138d9a11ce362f16c47a0812bbec50c38f4f0
soohyun08 <skim0829@outlook.com>
2022-12-23 14:56:57 +0900

    과정 저장

--- main.js
+++ main.js
@@ -122,3 +122,66 @@ function ani() {
 
 ani();
  */
+
+// 헤더 푸터
+const header = document.querySelector(".header");
+const headerHt = header.offsetTop;
+addEventListener("scroll", () => {
+  if (scrollY > headerHt) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+const lang = document.querySelector(".lang");
+
+lang.addEventListener("click", () => {
+  lang.classList.toggle("on");
+});
+
+const langKo = document.querySelector(".selectLang #ko");
+const langEn = document.querySelector(".selectLang #en");
+
+const selEn = document.querySelector("#langLi #selEn");
+const selKo = document.querySelector("#langLi #selKo");
+
+selEn.addEventListener("click", () => {
+  langKo.classList.remove("selected");
+  langEn.classList.add("selected");
+});
+selKo.addEventListener("click", () => {
+  langEn.classList.remove("selected");
+  langKo.classList.add("selected");
+});
+
+const goToBtn = document.querySelector(".scrollup");
+const fixedBtn = document.querySelector("#main_fixed_btn");
+const footer = document.querySelector("footer");
+
+addEventListener("scroll", () => {
+  if (scrollY >= 900) {
+    goToBtn.classList.add("scrollOn");
+    fixedBtn.style.bottom = "100px";
+  } else {
+    goToBtn.classList.remove("scrollOn");
+    fixedBtn.style.bottom = "53px";
+  }
+});
+// console.log(footer.offsetTop);
+// addEventListener("scroll", () => {
+//   if (scrollY >= footer.offsetHeight) {
+//     fixedBtn.classList.remove("poSticky");
+//   } else {
+//     fixedBtn.classList.add("poSticky");
+//   }
+// });
+
+/* 
+addEventListener("scroll", () => {
+  if (scrollY <= footer) {
+    fixedBtn.classList.remove("poFixed");
+  } else {
+    fixedBtn.classList.add("poRelative");
+  }
+}); */
